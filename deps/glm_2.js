cdbdbb79a5d199ecb484a9f2b8e3f990dff45963
dmitrymakhnin <dimamakhnin@gmail.com>
2017-10-05 23:42:21 -0400

    improved with glm

---
+++ deps/glm_2.js
@@ -0,0 +1,29 @@
+"use strict"
+
+var vec2 = function vec3() {
+    var value = new Float32Array(2);
+    this.value = value;
+}
+var vec3 = function vec3() {
+    var value = vec3_create.apply(null, arguments);
+    this.value = value;
+}
+var vec4 = function vec4() {
+    var value = vec4_create.apply(null, arguments);
+    this.value = value;
+}
+var mat2 = function mat2() {
+    var value = new Float32Array(4);
+    this.value = value;
+}
+var mat3 = function mat3() {
+    var value = mat3_create.apply(null, arguments);
+    this.value = value;
+}
+var mat4 = function mat4() {
+    var value = mat4_create.apply(null, arguments);
+    this.value = value;
+}
+
+
+
